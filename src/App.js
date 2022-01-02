12380464716d6a2142de56a702a9e94d59c27f66
Esteban Puma <puma.esteban18@gmail.com>
2022-01-01 22:22:25 -0300

    subo trabajo ciclo de vida de componentes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,67 @@
-import logo from './logo.svg';
+import React, { Component } from 'react'
+import {Gif} from './components/Gif'
+import TopBar from './components/TopBar';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+export default class App extends Component {
+
+  constructor(){
+    super();
+    this.state = {
+      gifs: [] 
+    }
+    this.addGif = this.addGif.bind(this)
+  }
+  componentDidMount(){
+    fetch('https://api.giphy.com/v1/gifs/trending?api_key=fYAY5UhrPTtE6c1OYJsgzzYNFQwyPRae&limit=4&rating=g')
+    .then(response => response.json())
+    .then(info => {
+      this.setState({
+        gifs: info.data
+      })
+    })
+    .catch(error => console.log(error))
+  }
+
+  componentDidUpdate(){
+
+  }
+
+  addGif(){
+    fetch('https://api.giphy.com/v1/gifs/random?api_key=fYAY5UhrPTtE6c1OYJsgzzYNFQwyPRae&tag=Lionel+Messi&rating=g')
+    .then(response=> response.json())
+    .then(info => this.setState({
+      gifs: [
+        ...this.state.gifs,
+        info.data
+      ]
+    }))
+    .catch(error => console.log(error))
+  }
+  
+
+  render() {
+    return (
+      <div className="container">
+      <TopBar addGif={this.addGif}/>
+
+      <div className="row text-center">
+
+      {
+        this.state.gifs.length === 0 
+        ?
+        <p>Cargando...</p>
+        :
+        this.state.gifs.map(gif => <Gif key={gif.id} image = {gif.images.original.url} titulo = {gif.title} />)
+        
+      }
+
+
+
+    </div>    
     </div>
-  );
+    )
+  }
 }
 
-export default App;
